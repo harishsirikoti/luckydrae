afb0aa182be0b3ab1e1c0d1a3f53b650ad2de40a
maeh1030 <120703075+maeh1030@users.noreply.github.com>
2022-12-16 11:16:52 +0530

    inital lucky draw

---
+++ js/luckydraw.js
@@ -0,0 +1,768 @@
+// function pickname(){
+    
+let data=[
+    {
+       Sno: 1,
+       Name: "RAVULAPALLI PRAKYATH",
+       Phone: 8309817961
+    },
+    {
+       Sno: 2,
+       Name: "NEDIMINTI SURAJ",
+       Phone: 9704691866
+    },
+    {
+       Sno: 3,
+       Name: "MUDAVATH BHANU PRASAD",
+       Phone: 6301113153
+    },
+    {
+       Sno: 4,
+       Name: "VIJENDAR BARIK",
+       Phone: 9640517447
+    },
+    {
+       Sno: 5,
+       Name: "MOHAMMED REYAN",
+       Phone: 9121847808
+    },
+    {
+       Sno: 6,
+       Name: "K. NAVEEN",
+       Phone: 9912392118
+    },
+    {
+       Sno: 7,
+       Name: "K. SHIVA KUMAR",
+       Phone: 6302141661
+    },
+    {
+       Sno: 8,
+       Name: "K. NIKHIL GOUD",
+       Phone: 9573376857
+    },
+    {
+       Sno: 9,
+       Name: "MOHD MEHBOOB",
+       Phone: 9989200286
+    },
+    {
+       Sno: 10,
+       Name: "DHARAVATH SANTHOSH",
+       Phone: 8104354077
+    },
+    {
+       Sno: 11,
+       Name: "BHUKYA SRIKANTH",
+       Phone: 9440072388
+    },
+    {
+       Sno: 12,
+       Name: "KAMMARI UMA MAHESHWARI",
+       Phone: 7680929122
+    },
+    {
+       Sno: 13,
+       Name: "K.MANOJ KUMAR",
+       Phone: 6303791865
+    },
+    {
+       Sno: 14,
+       Name: "B. NAGARAJU",
+       Phone: 9581509055
+    },
+    {
+       Sno: 15,
+       Name: "A. RAMAKRISHNA",
+       Phone: 6309306554
+    },
+    {
+       Sno: 16,
+       Name: "THUMKUNTA HAREESH",
+       Phone: 8331975852
+    },
+    {
+       Sno: 17,
+       Name: "A. KARTHIK",
+       Phone: 7993468058
+    },
+    {
+       Sno: 18,
+       Name: "B. SAI KUMAR",
+       Phone: 8341818781
+    },
+    {
+       Sno: 19,
+       Name: "GUGLOTH NAGESH",
+       Phone: 6303790273
+    },
+    {
+       Sno: 20,
+       Name: "BIKASH",
+       Phone: 9640258530
+    },
+    {
+       Sno: 21,
+       Name: "M.PAVAN KALYAN",
+       Phone: 9177996648
+    },
+    {
+       Sno: 22,
+       Name: "MADUGULA PRASANNA KUMAR",
+       Phone: 8520096985
+    },
+    {
+       Sno: 23,
+       Name: "K. N. V. S.S. P. MANIDEEP",
+       Phone: 9505532972
+    },
+    {
+       Sno: 24,
+       Name: "K DINAKAR",
+       Phone: 6302318201
+    },
+    {
+       Sno: 25,
+       Name: "HOKARANI NAVEEN",
+       Phone: 9133779663
+    },
+    {
+       Sno: 26,
+       Name: "BOMMA.PRAVEEN",
+       Phone: 7993474937
+    },
+    {
+       Sno: 27,
+       Name: "MOHAMMED SAMEER",
+       Phone: 6305781879
+    },
+    {
+       Sno: 28,
+       Name: "GANDHAMALLA JEEVAN GOUD",
+       Phone: 6302525986
+    },
+    {
+       Sno: 29,
+       Name: "DEEPAK SINGH",
+       Phone: 9912879498
+    },
+    {
+       Sno: 30,
+       Name: "NAKKA AKASH GOUD",
+       Phone: 9347980471
+    },
+    {
+       Sno: 31,
+       Name: "MOLUGU GOUTHAM",
+       Phone: 9014111341
+    },
+    {
+       Sno: 32,
+       Name: "M SOURAV TEJA",
+       Phone: 8367748843
+    },
+    {
+       Sno: 33,
+       Name: "B SHARANAPPA",
+       Phone: 9963786343
+    },
+    {
+       Sno: 34,
+       Name: "ERRAVENI MAHESH",
+       Phone: 6303790273
+    },
+    {
+       Sno: 35,
+       Name: "KHAMMAM SAI KUMAR",
+       Phone: 8096834059
+    },
+    {
+       Sno: 36,
+       Name: "KODURU PRANAV TEJA",
+       Phone: 7095886631
+    },
+    {
+       Sno: 37,
+       Name: "ROJA AHALYA GOUD",
+       Phone: 9573909763
+    },
+    {
+       Sno: 38,
+       Name: "E. SHREYANANDALAHARI",
+       Phone: 6301041773
+    },
+    {
+       Sno: 39,
+       Name: "KAMMARI SWETHA",
+       Phone: 9866060569
+    },
+    {
+       Sno: 40,
+       Name: "D. TEJASRI",
+       Phone: 9177552660
+    },
+    {
+       Sno: 41,
+       Name: "POLOJU HARINI",
+       Phone: 7095702213
+    },
+    {
+       Sno: 42,
+       Name: "KATHA DURGA BHAVANI",
+       Phone: 9121847808
+    },
+    {
+       Sno: 43,
+       Name: "KOTA AMBIKA",
+       Phone: 6301041773
+    },
+    {
+       Sno: 44,
+       Name: "BANDARI RAJESHWARI",
+       Phone: 9866060569
+    },
+    {
+       Sno: 45,
+       Name: "YEGAMATI GOUTHAMI",
+       Phone: 9177552660
+    },
+    {
+       Sno: 46,
+       Name: "NATAKARI MANISHA",
+       Phone: 9912392118
+    },
+    {
+       Sno: 47,
+       Name: "CHINTHALA BOOMIKA",
+       Phone: 6302141661
+    },
+    {
+       Sno: 48,
+       Name: "PUSALA VAISHNAVI",
+       Phone: 9573376857
+    },
+    {
+       Sno: 49,
+       Name: "NETROMBAK LIKITHA",
+       Phone: 9177996648
+    },
+    {
+       Sno: 50,
+       Name: "CHIRUNAGULA ANKITHA",
+       Phone: 9989200286
+    },
+    {
+       Sno: 51,
+       Name: "LINGAMPALLI SWATI",
+       Phone: 8104354077
+    },
+    {
+       Sno: 52,
+       Name: "AKSHITA CHAUDHARY",
+       Phone: 9440072388
+    },
+    {
+       Sno: 53,
+       Name: "GURU DEVI CHANDANA",
+       Phone: 7680929122
+    },
+    {
+       Sno: 54,
+       Name: "ARCOT SITARAM SHIVA TEJA",
+       Phone: 6303791865
+    },
+    {
+       Sno: 55,
+       Name: "PRODDUVAKA PRAVEEN",
+       Phone: 9581509055
+    },
+    {
+       Sno: 56,
+       Name: "EDIGALLA VAMSHI",
+       Phone: 6309306554
+    },
+    {
+       Sno: 57,
+       Name: "REVELLI DHANRAJ GOUD",
+       Phone: 8331975852
+    },
+    {
+       Sno: 58,
+       Name: "YEDLA DHANRAJ",
+       Phone: 7993468058
+    },
+    {
+       Sno: 59,
+       Name: "DEVARASHETTY SAI PRASAD",
+       Phone: 8341818781
+    },
+    {
+       Sno: 60,
+       Name: "ARAPALLY ABHISHEK",
+       Phone: 9640258530
+    },
+    {
+       Sno: 61,
+       Name: "INUGURTHI MURALI KRISHNA",
+       Phone: 7095702213
+    },
+    {
+       Sno: 62,
+       Name: "BOMMERA VENKATESH",
+       Phone: 7993474937
+    },
+    {
+       Sno: 63,
+       Name: "BANDAMEEDI ARAVIND KUMAR",
+       Phone: 8520096985
+    },
+    {
+       Sno: 64,
+       Name: "VALLURI SAI KUMAR",
+       Phone: 9505532972
+    },
+    {
+       Sno: 65,
+       Name: "PAGADALA ISAAC",
+       Phone: 6305781879
+    },
+    {
+       Sno: 66,
+       Name: "JONNADA BHARATH BABU",
+       Phone: 9912879498
+    },
+    {
+       Sno: 67,
+       Name: "P LOKESH GOUD",
+       Phone: 9014111341
+    },
+    {
+       Sno: 68,
+       Name: "KORA SAI KUMAR",
+       Phone: 6302525986
+    },
+    {
+       Sno: 69,
+       Name: "SEELAM VAMSI",
+       Phone: 9347980471
+    },
+    {
+       Sno: 70,
+       Name: "NAGULA SAI KUMAR",
+       Phone: 6302318201
+    },
+    {
+       Sno: 71,
+       Name: "SHERY KRANTHI KUMAR",
+       Phone: 8367748843
+    },
+    {
+       Sno: 72,
+       Name: "KATLA UMA SHANKAR",
+       Phone: 9133779663
+    },
+    {
+       Sno: 73,
+       Name: "KADUKUNTLA SUKUMAR REDDY",
+       Phone: 9573909763
+    },
+    {
+       Sno: 74,
+       Name: "BHUKYA RAJKUMAR",
+       Phone: 7095886631
+    },
+    {
+       Sno: 75,
+       Name: "BORADI TIRUMALA RAO",
+       Phone: 9963786343
+    },
+    {
+       Sno: 76,
+       Name: "MADIPADIGE RAKESH KUMAR",
+       Phone: 8374451561
+    },
+    {
+       Sno: 77,
+       Name: "RAJPUT RAHUL PRASAD",
+       Phone: 7993641861
+    },
+    {
+       Sno: 78,
+       Name: "AVULA NANDU",
+       Phone: 9963338033
+    },
+    {
+       Sno: 79,
+       Name: "NAIKOTI NIKHIL KUMAR",
+       Phone: 6303873731
+    },
+    {
+       Sno: 80,
+       Name: "PITTA NAGENDAR REDDY",
+       Phone: 8328492543
+    },
+    {
+       Sno: 81,
+       Name: "KETHAVATH RAJESH",
+       Phone: 9989948354
+    },
+    {
+       Sno: 82,
+       Name: "PANDITI SHYAM PRASAD",
+       Phone: 6303518289
+    },
+    {
+       Sno: 83,
+       Name: "KOTHAKONDA MAHESH BABU",
+       Phone: 8639671581
+    },
+    {
+       Sno: 84,
+       Name: "PEDDAGOLLA ARUN KUMAR",
+       Phone: 9550701431
+    },
+    {
+       Sno: 85,
+       Name: "RALLABANDI M RAHUL",
+       Phone: 8341252987
+    },
+    {
+       Sno: 86,
+       Name: "KORE SAIKUMAR",
+       Phone: 9666849580
+    },
+    {
+       Sno: 87,
+       Name: "CH AKHIL KUMAR",
+       Phone: 9133665630
+    },
+    {
+       Sno: 88,
+       Name: "SHIKHA TIWARI",
+       Phone: 9381246493
+    },
+    {
+       Sno: 89,
+       Name: "YOGGU SAHITHY",
+       Phone: 8686298261
+    },
+    {
+       Sno: 90,
+       Name: "BOINOLLA SREELATHA",
+       Phone: 9912781403
+    },
+    {
+       Sno: 91,
+       Name: "ANNE MAMATHA ",
+       Phone: 9515716665
+    },
+    {
+       Sno: 92,
+       Name: "CHANDRAGAS MAHESHWARI",
+       Phone: 9121445576
+    },
+    {
+       Sno: 93,
+       Name: "BOMARTHI REVATHI",
+       Phone: 9398403004
+    },
+    {
+       Sno: 94,
+       Name: "CHEVALLA POOJA",
+       Phone: 9866758601
+    },
+    {
+       Sno: 95,
+       Name: "KOTTALA NGARANI",
+       Phone: 9010008145
+    },
+    {
+       Sno: 96,
+       Name: "GUMPALA VARALAXMI",
+       Phone: 9032216551
+    },
+    {
+       Sno: 97,
+       Name: "GURKA SHILPA",
+       Phone: 9502328718
+    },
+    {
+       Sno: 98,
+       Name: "SONIA SWAIN ",
+       Phone: 9912625476
+    },
+    {
+       Sno: 99,
+       Name: "KOTTAPALLI HIMABINDU",
+       Phone: 8106213935
+    },
+    {
+       Sno: 100,
+       Name: "THANDA RAJANIKANTH"
+    },
+    {
+       Sno: 101,
+       Name: "METTU ABHISHEK REDDY ",
+       Phone: 9502084162
+    },
+    {
+       Sno: 102,
+       Name: "POLKAM SRIDEVI ",
+       Phone: 9949650836
+    },
+    {
+       Sno: 103,
+       Name: "ESLAVATH SRIKANTH ",
+       Phone: 7095987078
+    },
+    {
+       Sno: 104,
+       Name: "KAMISHETTI SATHISH",
+       Phone: 9542703542
+    },
+    {
+       Sno: 105,
+       Name: "SILPURI KANAKA MAHALAXMI",
+       Phone: 7995420985
+    },
+    {
+       Sno: 106,
+       Name: "KOSINAPALLY HARISH ",
+       Phone: 6303402635
+    },
+    {
+       Sno: 107,
+       Name: "POTHU MADHU",
+       Phone: 9908164704
+    },
+    {
+       Sno: 108,
+       Name: "MUTHKURU ADARSH KUMAR ",
+       Phone: 9618359074
+    },
+    {
+       Sno: 109,
+       Name: "MUDAVATH ASHOK",
+       Phone: 9603158746
+    },
+    {
+       Sno: 110,
+       Name: "GAANDLA VIJAY KUMAR ",
+       Phone: 7893057525
+    },
+    {
+       Sno: 111,
+       Name: "VANARASI RAMESH ",
+       Phone: 8096042540
+    },
+    {
+       Sno: 112,
+       Name: "KANKDEKAR KARTHIK",
+       Phone: 7207591334
+    },
+    {
+       Sno: 113,
+       Name: "NAGPURI SHEKAR ",
+       Phone: 7093412972
+    },
+    {
+       Sno: 114,
+       Name: "SEELAM CHIRUDEEP",
+       Phone: 8184901037
+    },
+    {
+       Sno: 115,
+       Name: "POOSA BANDARI SHIVA KUMAR",
+       Phone: 7013610963
+    },
+    {
+       Sno: 116,
+       Name: "PANDUGA SAI KUMAR",
+       Phone: 9848968411
+    },
+    {
+       Sno: 117,
+       Name: "GORU VAMSI KRISHNA",
+       Phone: 9603216066
+    },
+    {
+       Sno: 118,
+       Name: "RAJPUT ABHISHEK PRASAD",
+       Phone: 7993656562
+    },
+    {
+       Sno: 119,
+       Name: "A HARHSA VARDHAN",
+       Phone: 9700221578
+    },
+    {
+       Sno: 120,
+       Name: "KUNCHARAM SHIVA KUMAR",
+       Phone: 9059200993
+    },
+    {
+       Sno: 121,
+       Name: "GOLLAPALLY SAGAR ",
+       Phone: 7036775003
+    },
+    {
+       Sno: 122,
+       Name: "PATTHI VIJAYKANTH",
+       Phone: 9553005491
+    },
+    {
+       Sno: 123,
+       Name: "KANNEBOINA SRIKANTH",
+       Phone: 9553120601
+    },
+    {
+       Sno: 124,
+       Name: "MALLAM NAVEEN ",
+       Phone: 9963235371
+    },
+    {
+       Sno: 125,
+       Name: "VUMMEANTHALA VENKATA SEETHA RAMA RAO",
+       Phone: 8179879591
+    },
+    {
+       Sno: 126,
+       Name: "BATHINI PAVAN KUMAR",
+       Phone: 9705698817
+    },
+    {
+       Sno: 127,
+       Name: "BANOTH VINOD NAIK",
+       Phone: 9133641207
+    },
+    {
+       Sno: 128,
+       Name: "NAKKAPALLI PRASHANTH",
+       Phone: 8106008973
+    },
+    {
+       Sno: 129,
+       Name: "BUTCHALA RAMAKRISHNA",
+       Phone: 8184800579
+    },
+    {
+       Sno: 130,
+       Name: "BATHULA SANDEEP",
+       Phone: 7036350226
+    },
+    {
+       Sno: 131,
+       Name: "AZMEERA NAVEEN",
+       Phone: 8281675215
+    },
+    {
+       Sno: 132,
+       Name: "BURRA RAHUL GOUD "
+    },
+    {
+       Sno: 133,
+       Name: "RAPOLU KRISHNA PRASAD",
+       Phone: 7036878295
+    },
+    {
+       Sno: 134,
+       Name: "CHALLA VISHNU SHIVAKUMAR",
+       Phone: 8341018624
+    },
+    {
+       Sno: 135,
+       Name: "SIRIKOTTI HARISH ",
+       Phone: 9652725760
+    },
+    {
+       Sno: 136,
+       Name: "K NAVEEN KUMAR REDDY ",
+       Phone: 7661851441
+    },
+    {
+       Sno: 137,
+       Name: "BOYINI DIVYA",
+       Phone: 8374346875
+    },
+    {
+       Sno: 138,
+       Name: "SAGGAM SAI KUMAR",
+       Phone: 8179930241
+    },
+    {
+       Sno: 139,
+       Name: "VUYALA RAKESH",
+       Phone: 9912785917
+    },
+    {
+       Sno: 140,
+       Name: "K.SAI SUDHEER",
+       Phone: 9515731676
+    },
+    {
+       Sno: 141,
+       Name: "ALIJALA RAMUDU",
+       Phone: 9133914873
+    },
+    {
+       Sno: 142,
+       Name: "ALIJALA LAXMAN",
+       Phone: 9133884857
+    },
+    {
+       Sno: 143,
+       Name: "A CHANDRASHEKAR",
+       Phone: 8121801522
+    },
+    {
+       Sno: 144,
+       Name: "B.NIKHIL",
+       Phone: 9515731676
+    },
+    {
+       Sno: 145,
+       Name: "E CHAKRADHAR",
+       Phone: 8328078879
+    },
+    {
+       Sno: 146,
+       Name: "E MADHU",
+       Phone: 9652531635
+    },
+    {
+       Sno: 147,
+       Name: "G RAHUL SWAMY",
+       Phone: 9705222002
+    },
+    {
+       Sno: 148,
+       Name: "M SAE PRANAW",
+       Phone: 9440212117
+    },
+    {
+       Sno: 149,
+       Name: "A KALYAN KUMAR",
+       Phone: 8008597345
+    }
+]
+
+
+ 
+  function pickname(){
+    var k=  setInterval(()=>{
+        let y=data.length;
+        let x = Math.floor((Math.random() * y) );
+    document.getElementById("winner").innerHTML =x+1;
+    document.getElementById("demo2").innerHTML=data[x].Name+"<br>";
+    },100)
+    setTimeout(function( ) { clearInterval( k ); }, 10000);
+document.getElementById("win").style.display="block";
+document.getElementById("center").style.display="block";
+document.getElementById("center3").style.display="block";
+document.getElementById("win").style.display="block";
+    
+
+}
+
+   
+   
